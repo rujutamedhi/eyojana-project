69b1c0662e1a9ef07b4d173bcd02bcaae27887be
Shravani Anil Patil <145674851+ShravaniAnilPatil@users.noreply.github.com>
2024-09-17 11:38:04 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://ShravaniAnilPatil:Shweta2509@cluster0.tspoa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+    await mongoose.connect('mongodb+srv://ShravaniAnilPatil:<password>@cluster0.tspoa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
