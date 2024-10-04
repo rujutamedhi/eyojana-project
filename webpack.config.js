e5cd3c330a71ac5399efb751105f294a07ac59b4
IT <madhurajangle2004@gmail.com>
2024-10-04 20:47:55 +0530

    correction app.js

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  // Your existing configuration
+  resolve: {
+    fallback: {
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      stream: require.resolve('stream-browserify'),
+      zlib: require.resolve('browserify-zlib'),
+      util: require.resolve('util/'),
+      assert: require.resolve('assert/'),
+      url: require.resolve('url/')
+    }
+  }
+};
