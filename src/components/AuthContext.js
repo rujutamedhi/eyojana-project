e477dc20607bea7314367c735da24dcd485d4b46
rujutamedhi <rujutamedhi@gmail.com>
2024-10-07 20:25:06 +0530

    refresh issue resolved

--- src/components/AuthContext.js
+++ src/components/AuthContext.js
@@ -1,11 +1,14 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState,useEffect } from 'react';
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [email, setEmail] = useState('');
-
+  useEffect(() => {
+    const token = localStorage.getItem("authToken");
+    setIsLoggedIn(!!token); // Set logged in state based on presence of token
+}, []);
   return (
     <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, email, setEmail }}>
       {children}
